534f6be1bf5840f9654c8d63f5f9420976b88a10
Lalit Patel <lalitpatel@users.noreply.github.com>
2021-08-17 11:48:25 +0530

    Website v2.0

--- README.md
+++ README.md
@@ -1,11 +1,38 @@
-# website
+<h1 align="center">SiegeEngineers.org Website</h1>
+
 The main Siege Engineers website.
 
-# Adding projects
+Requires Node 14+
 
-The project tiles are arranged in an alternating 8 - 4 / 4 - 8 pattern. 
-Add the new project to a free tile of a suitable size, 
-tell us a bit about the project,
-make the title a link to the project if possible,
-and pick a suitable colour that you like and that is not a neighboring colour. 
-You can use any of the eight AoEII player colours as well as dark gray.
+## Commands
+```sh
+# Install Gatsby CLI
+npm install -g gatsby-cli
+
+# install dependencies
+npm install
+
+# Development
+gatsby develop
+
+# Production Build
+gatsby build
+
+# Serve the build locally
+gatsby serve
+```
+
+## Other Commands
+```sh
+# working locally
+npm run dev
+
+# generate build
+npm run build
+
+# format code
+npm run format
+
+# lint code
+npm run lint
+```
---
+++ data/site-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteTitle: 'Siege Engineers',
+  siteDescription: 'Siege Engineers is a development collective around Age of Empires II',
+  authorName: 'Siege Engineers Team',
+  twitterUsername: 'siegeengineers',
+  authorAvatar: 'avatar.png', // file in content/images
+  defaultLang: 'en',
+  authorDescription: `Siege Engineers is a development collective around Age of Empires II`,
+  siteUrl: 'https://siegeengineers.org/',
+  // Prefixes all links. For cases when deployed to siegeengineers.org/website-directory/
+  pathPrefix: '/', // Note: it must *not* have a trailing slash.
+  siteCoverImage: 'cover.jpg', // file in content/images
+  background_color: '#ffffff',
+  themeColor: '#ffdc4e',
+  display: 'browser',
+  icon: 'content/images/se-logo.png',
+  postsPerPage: 5,
+  blogPostPathPrefix: 'post', // /post/post-slug
+  navBarLogo: 'se-logo.png' //  (leave empty to disable: '')
+};
