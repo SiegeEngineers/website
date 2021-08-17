00c2836dac8dec1512bfc4956ebd430e0c8bbe5b
Lalit Patel <lalitpatel@users.noreply.github.com>
2021-08-17 14:38:31 +0530

    Fix image for social sharing

--- data/site-config.js
+++ data/site-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   siteTitle: 'Siege Engineers',
-  siteDescription: 'Siege Engineers is a development collective around Age of Empires II',
-  authorName: 'Siege Engineers Team',
-  twitterUsername: 'siegeengineers',
-  authorAvatar: 'avatar.png', // file in content/images
-  defaultLang: 'en',
-  authorDescription: `Siege Engineers is a development collective around Age of Empires II`,
+  siteDescription: 'Siege Engineers is a development collective around Age of Empires II esports.',
   siteUrl: 'https://siegeengineers.org/',
+  siteCoverImage: 'se-banner.png', // file in content/images
+  defaultLang: 'en',
+  authorName: 'Siege Engineers Team',
+  authorAvatar: 'avatar.png', // file in content/images
+  authorDescription: `Siege Engineers is a development collective around Age of Empires II esports.`,
+  twitterUsername: 'siegeengineers',
   // Prefixes all links. For cases when deployed to siegeengineers.org/website-directory/
   pathPrefix: '/', // Note: it must *not* have a trailing slash.
-  siteCoverImage: 'cover.jpg', // file in content/images
   background_color: '#ffffff',
   themeColor: '#ffdc4e',
   display: 'browser',
