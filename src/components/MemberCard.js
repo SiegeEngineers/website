ad6e9d1b1c9b31ff3cda726f14f3447491d97bae
HSZemi <hszemi@users.noreply.github.com>
2024-07-19 22:36:41 +0200

    Update board members

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -8,14 +8,13 @@ const MemberCard = props => {
     <article className="media">
       <figure className="media-left mb-0 mx-4 image is-64x64">
         <a href={`https://discordapp.com/users/${props.discordId}`} target="_blank" rel="noreferrer">
-          <img src={avatarUrl} className="is-rounded" alt={props.username} />
+          <img src={avatarUrl} className="is-rounded" alt={props.name} />
         </a>
       </figure>
       <div className="media-content">
         <div className="content pt-1">
           <a href={`https://discordapp.com/users/${props.discordId}`} target="_blank" rel="noreferrer">
-            <span className="title is-size-5">{props.username}</span>
-            <span className="subtitle has-text-grey-light">#{props.tag}</span>
+            <span className="title is-size-5">{props.name}</span><span> </span>
           </a>
           <p className="has-text-grey mt-1">{props.children}</p>
         </div>
